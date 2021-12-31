d3363cd293c74eaace3eedd71ac3b363a6348397
Alex Vergara <pyzyryab@utanota.com>
2021-12-31 01:33:14 +0100

    Texture loader impl. Reexported loader module as gdloader

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use gdnative::prelude::*;
 
-pub mod loader;
+mod loader;
+
+pub use loader::loader as gdloader;
 
 /// Just a health_checker to test if the project it's correctly
 /// downloaded by Cargo from GitHub in another Godot-Rust project
--- src/loader/loader.rs
+++ src/loader/loader.rs
@@ -1,13 +1,12 @@
 use gdnative::prelude::*;
 
-/// Loads a Godot resource (a scene, an asset...) from a given path
-/// absolute to res://
+/// Loads a Godot Node by path, being absolute to res://
 /// 
 /// Returns a temporary reference to a node `TRef<'static, Node>`.
 /// 
 /// An `&str` like `godot/my_folder/my_scene.tscn` provided on the argument
 /// w'd be used to `res://godot/my_folder/my_scene.tscn`
-pub fn load_resource(path: &str) -> TRef<'static, Node> {
+pub fn load_node(path: &str) -> TRef<'static, Node> {
     // Concatenate the path to the Godot's root path identifier
     let path_from_res = &("res://".to_string() + path)[..];
 
@@ -22,4 +21,26 @@ pub fn load_resource(path: &str) -> TRef<'static, Node> {
         .unwrap()
         .assume_safe() 
     }
+}
+
+
+/// Loads a Godot Texture by path, being absolute to res://
+/// 
+/// Returns a reference to a node `Ref<Texture>`.
+/// 
+/// An `&str` like `godot/my_folder/my_scene.tscn` provided on the fn argument
+/// w'd be used to `res://godot/my_folder/my_scene.tscn`
+pub fn load_texture(path: &str) -> Ref<Texture> {
+    // Concatenate the path to the Godot's root path identifier
+    let path_from_res = &("res://".to_string() + path)[..];
+
+    // Returning the Node
+    return unsafe { ResourceLoader::godot_singleton()
+        .load(path_from_res, "", false)
+        .unwrap()
+        .assume_safe()
+        .cast::<Texture>()
+        .unwrap()
+        .assume_shared()
+    };
 }
