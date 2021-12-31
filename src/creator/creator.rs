af837475dfe7670d8cb83eb448ad99dae8b5e134
Alex Vergara <pyzyryab@utanota.com>
2021-12-31 02:13:47 +0100

    Sprite creator can set now the name of the resource to Godot.

--- src/creator/creator.rs
+++ src/creator/creator.rs
@@ -1,14 +1,16 @@
 use gdnative::prelude::*;
 use crate::loader::loader::load_texture;
 
-/// Returns a new instanciated `TRef<Sprite>` with an asset already loaded,
+/// Returns a new instanciated `TRef<Sprite>` with an asset as a GFX already loaded,
 /// simplifiying the process of create a new sprite, load the asset as a resource
 /// and setting it as a texture of the sprite in a single method call
-fn sprite_with_asset(path: &str) -> TRef<Sprite> {
+pub fn sprite_with_asset(sprite_node_name: &str, path: &str) -> TRef<'static, Sprite> {
     // Creates a new TRef to a sprite
     let sprite = unsafe { Sprite::new().assume_shared().assume_safe() };
     // Loading the GFX assets
     let sprite_asset = load_texture(path);
+    // Setting a name identifier (to Godot) as a node name identifier
+    sprite.set_name(sprite_node_name);
     // Attaching it to the sprite
     sprite.set_texture(sprite_asset);
     // returning the sprite
