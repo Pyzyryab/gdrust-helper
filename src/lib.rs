027dedda0c8e29014cb3454e083537e3160ff81b
Pyzyryab <elgithub_10>
2022-01-22 00:29:28 +0100

    Input mapping it's now reexported

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub use loader::loader as gdloader;
 pub use creator::creator as gdcreator;
 pub use motion::motion as gdmotion;
 pub use config::game_config as gdconfig;
+pub use config::input_mapping as gdkeybinder;
 
 /// Just a health_checker to test if the project it's correctly
 /// downloaded by Cargo from GitHub in another Godot-Rust project
