6aa0199fa01745819d718e66b77fb432c4d7aa71
Pyzyryab <pyzyryab@tutanota.com>
2022-01-22 12:07:08 +0100

    The KeysWithMouse trait now manages itself to allocate a Vector2 for the motion and a &Input for the input instead of receiving it by param)

--- src/motion/motion.rs
+++ src/motion/motion.rs
@@ -1,6 +1,7 @@
-//  * This file provides several traits that, when implemented on a `gdrust` object, 
-//  * automatically provides some kind of **2D** movement, like normal 2D motion with keys, 
-//  * with keys and orientation with mouse, Pokémon like tile-based movement...
+///  * This file provides several traits that, when implemented on a `gdrust` object, 
+///  * automatically provides some kind of **2D** movement, like normal 2D motion with keys, 
+///  * with keys and orientation with mouse, Pokémon like tile-based movement, 
+///  * mechanical 2D simple motion...
 
 use gdnative::prelude::*;
 
@@ -14,13 +15,12 @@ use crate::config::{
 /// with the keyboard's keys (provided by the Godot's Input singleton) and the player's orientation
 /// (or where it's looking) control by tracking where the mouse it's located on the screen.
 /// 
-/// Method need to have an `input` Godot Singleton, in order to avoid the overhead fn call on every
-/// `_process` or `_physics_process` iteration, or to avoi. 
+/// Method needs to receive a couple of structs, one representing the actual player configuration
+/// (like it's SPEED, MAX SPEED, FRICTION constant values) and another that will contains
+/// constants that holds the keybindings configured on the Godot's editor
 pub trait KeysMotionMouseDirection {
     fn move_character(
-        self: &mut Self,
-        input: &Input,
-        motion: &mut Vector2,
+        self: &Self,
         // TODO The next two params should be contained on a bigger struct, 'cause the game config 
         // probably will increase the number of structs needed to hold it's various configurations.
         // So, having just one, for example, `game_config: GameConfiguration` struct that will hold
@@ -29,8 +29,13 @@ pub trait KeysMotionMouseDirection {
         // or we can right it based in Rust optional type for initialization, and the drawback just 
         // will be having more fun unwraping data.
         player_config: CharacterConfiguration,
-        keybinding: MotionKeybindings
-    ) {
+        keybinding: MotionKeybindings 
+    ) -> Vector2 {
+
+        // Allocate variables to track the actual motion based on an input received
+        let input: &Input = Input::godot_singleton();
+        let mut motion: Vector2 = Vector2::ZERO;
+
         // Control the vertical motion
         if Input::is_action_pressed( input, keybinding.get_up_keybinding(), false ) 
             && !Input::is_action_pressed( input, keybinding.get_down_keybinding(), false ) {
@@ -55,6 +60,9 @@ pub trait KeysMotionMouseDirection {
         }
         else {
             motion.x = 0.0;
-        }   
+        }
+
+        // Returns the result of the computation
+        motion
     }
 }
